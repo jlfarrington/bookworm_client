d0e632e41b4527082c52eb08098ea5bf20932434
Jonny Rene Calderon <jonniesta@jonnys-air.attlocal.net>
2021-02-25 19:37:15 -0500

    Jonny latest push

--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -24,7 +24,7 @@ const Sitebar = (props) => {
             <Collapse isOpen={isOpen} navbar>
                 <Nav className="ml-auto" navbar>
                     <NavItem>
-                        <Button id="button" onClick={props.clickLogout}>Logout</Button>
+                        <Button id="logoutbutton" onClick={props.clickLogout}>Logout</Button>
                     </NavItem>
                 </Nav>
             </Collapse>
